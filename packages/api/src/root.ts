464424c700d82d93f592ef3df335b1e370ab9088
Bludvikk <cleverdmasoy@gmail.com>
2023-04-06 19:39:22 +0800

    test-endpoint

--- packages/api/src/root.ts
+++ packages/api/src/root.ts
@@ -1,10 +1,10 @@
 import { authRouter } from "./router/auth";
-import { postRouter } from "./router/post";
+import { salesRouter } from "./router/post";
 import { router } from "./trpc";
 
 
 export const appRouter = router({
-  post: postRouter,
+  post: salesRouter,
   auth: authRouter,
 
 });
